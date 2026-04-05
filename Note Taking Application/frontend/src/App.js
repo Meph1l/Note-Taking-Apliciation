b490ef6049cbfbeb039d90d19e72434c19203c4b
wang yuhao <32587294+w3066024151@users.noreply.github.com>
2026-04-04 21:32:38 -0400

    Update App.js

--- Note Taking Application/frontend/src/App.js
+++ Note Taking Application/frontend/src/App.js
@@ -1,15 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import TagResearch from "./pages/TagResearch";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
-      </Routes>
+        <Routes>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/tag-research" element={<TagResearch/>}/>
+        </Routes>
       </BrowserRouter>
     </div>
   );
